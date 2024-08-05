230acd77bf0eda4580ddfb230c44d10ae7cd3b0f
Rohit2853 <rohitsunilgiratkar2853@gmail.com>
2024-08-06 01:35:55 +0530

    modified analysis content

--- backend/controllers/analysis.js
+++ backend/controllers/analysis.js
@@ -1,17 +1,34 @@
 const ExpenseSchema = require("../models/ExpenseModel")
 const IncomeSchema = require("../models/IncomeModel")
+
+const getStartDateofPreviousYear = ()=>{
+    const now = new Date();
+    const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+    const startOfPreviousYear = new Date(startOfMonth.setFullYear(startOfMonth.getFullYear() - 1));
+    return startOfPreviousYear;
+}
+const getMonthYearString = (date) => {
+    const options = { year: 'numeric', month: 'long' };
+    return new Intl.DateTimeFormat('en-US', options).format(date);
+};
 exports.getanalysis = async(req,res)=>{
     try{
+        const startDate = getStartDateofPreviousYear();
+        const endDate = new Date();
         const incomes = await IncomeSchema.find();
         const expenses = await ExpenseSchema.find();
+        const oneYearIncomes = await IncomeSchema.find({date : {$gte :startDate,$lte:endDate}});
+        const oneYearExpenses = await ExpenseSchema.find({date : {$gte :startDate,$lte:endDate}});
+
+
         const data = [];
         if(!incomes.length && !expenses.length){
             res.status(401).json({message:'Insuffcient Data to show Analysis'},data=data);
         }
-        const totalIncome = incomes.reduce((a,b)=> a+b.amount,0);
-        const totalExpense = expenses.reduce((a,b)=>a+b.amount,0);
-        const averageIncome = totalIncome/(incomes.length);
-        const averageExpense = totalExpense/(expenses.length);
+        // const totalIncome = incomes.reduce((a,b)=> a+b.amount,0);
+        // const totalExpense = expenses.reduce((a,b)=>a+b.amount,0);
+        // const averageIncome = totalIncome/(incomes.length);
+        // const averageExpense = totalExpense/(expenses.length);
 
         const incomeSummary = incomes.reduce((acc,curr)=>{
             if(!acc[curr.category]){
@@ -55,7 +72,7 @@ exports.getanalysis = async(req,res)=>{
         incomes.forEach(t=>{
             const transactionDate = new Date(t.date);
             const monthDiff = currentMonth - transactionDate.getMonth() + (currentYear -transactionDate.getFullYear())*12;
-            if(monthDiff>=0 && monthDiff<=12){
+            if(monthDiff>=0 && monthDiff<=11){
                 monthlyIncomeBreakDown[monthDiff].amount += t.amount;
             }
         });
@@ -63,16 +80,40 @@ exports.getanalysis = async(req,res)=>{
         expenses.forEach(t=>{
             const transactionDate = new Date(t.date);
             const monthDiff = currentMonth - transactionDate.getMonth() + (currentYear -transactionDate.getFullYear())*12;
-            if(monthDiff>=0 && monthDiff<=12){
+            if(monthDiff>=0 && monthDiff<=11){
                 monthlyExpenseBreakDown[monthDiff].amount += t.amount;
             }
         });
-        console.log("IncomeSummary", incomeSummary)
-        console.log("ExpenseSummary", expenseSummary)
-        console.log("monthlyIncomeBreakdown", monthlyIncomeBreakDown)
-        console.log("monthlyExpenseBreakdown", monthlyExpenseBreakDown)
+
+
+        //it will have the monthly category wise data for one year
+        const monthlyIncomeSummary = {};
+        const monthlyExpenseSummary = {};
+        oneYearIncomes.map((income)=>{
+            const monthYear = getMonthYearString(new Date(income.date));
+            if(!monthlyIncomeSummary[monthYear]){
+                monthlyIncomeSummary[monthYear]={};
+            }
+            if(!monthlyIncomeSummary[monthYear][income.category]){
+                monthlyIncomeSummary[monthYear][income.category]=0;
+            }
+            monthlyIncomeSummary[monthYear][income.category] += income.amount;
+        })
+
+        oneYearExpenses.map((expense)=>{
+            const monthYear = getMonthYearString(new Date(expense.date));
+            if(!monthlyExpenseSummary[monthYear]){
+                monthlyExpenseSummary[monthYear]={};
+            }
+            if(!monthlyExpenseSummary[monthYear][expense.category]){
+                monthlyExpenseSummary[monthYear][expense.category]=0;
+            }
+            monthlyExpenseSummary[monthYear][expense.category] += expense.amount;
+        })
+
+
         res.status(200).json({incomeSummary,expenseSummary,
-            monthlyExpenseBreakDown,monthlyIncomeBreakDown});
+            monthlyExpenseBreakDown,monthlyIncomeBreakDown,monthlyIncomeSummary,monthlyExpenseSummary});
     }catch(error){
         console.log(error)
         res.status(401).json({message:'Could not perform analysis'})
--- frontend/MoneyMate/src/components/Analysis/Analysis.jsx
+++ frontend/MoneyMate/src/components/Analysis/Analysis.jsx
@@ -2,16 +2,22 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { analysis } from '../../utils/Icons'
 import styled from 'styled-components';
-import { InnerLayout } from '../../styles/Layouts';
 import PieChart from '../Chart/PieChart';
 import BarChart from '../Chart/BarChart';
+import DoughnutChart from '../Chart/DoughnutChart';
 const BASE_URL = "http://localhost:5000/api/v1/";
 
 function Analysis() {
-    const[incomeSummary,setIncomeSummary] = useState({});
-    const[expenseSummary,setExpenseSummary] = useState({});
-    const[incomeDistribution,setIncomeDistribution] = useState([]);
-    const[expenseDistribution,setExpenseDistribution] = useState([]);
+    const [incomeSummary,setIncomeSummary] = useState({});
+    const [expenseSummary,setExpenseSummary] = useState({});
+    const [incomeDistribution,setIncomeDistribution] = useState([]);
+    const [expenseDistribution,setExpenseDistribution] = useState([]);
+    const [monthlyIncomeSummary,setMonthlyIncomeSummary]=useState({});
+    const [monthlyExpenseSummary,setMonthlyExpenseSummary]=useState({});
+    const [incomeMonth,setIncomeMonth] = useState('');
+    const [expenseMonth,setExpenseMonth] = useState('');
+    const [activeIncomeMonth, setActiveIncomeMonth] = useState('');
+    const [activeExpenseMonth, setActiveExpenseMonth] = useState('');
     const[error,setError] = useState(0);
     useEffect(()=>{
         async function fetchData(){
@@ -21,6 +27,8 @@ function Analysis() {
                 setExpenseSummary(response.data.expenseSummary);
                 setIncomeDistribution(response.data.monthlyIncomeBreakDown.slice().reverse());
                 setExpenseDistribution(response.data.monthlyExpenseBreakDown.slice().reverse());
+                setMonthlyIncomeSummary(response.data.monthlyIncomeSummary);
+                setMonthlyExpenseSummary(response.data.monthlyExpenseSummary);
                 setError(0)
                 // console.log(response.data);
             }catch(error){
@@ -33,20 +41,57 @@ function Analysis() {
     },[]);
     useEffect(() => {
         console.log("Updated incomeSummary:", incomeSummary);
-        }, [incomeSummary]);
+    }, [incomeSummary]);
 
-        useEffect(() => {
+    useEffect(() => {
         console.log("Updated expenseSummary:", expenseSummary);
-        }, [expenseSummary]);
+    }, [expenseSummary]);
 
-        useEffect(() => {
+    useEffect(() => {
         console.log("Updated incomeDistribution:", incomeDistribution);
-        }, [incomeDistribution]);
+    }, [incomeDistribution]);
 
-        useEffect(() => {
+    useEffect(() => {
         console.log("Updated expenseDistribution:", expenseDistribution);
-        }, [expenseDistribution]);
-    
+    }, [expenseDistribution]);
+
+    useEffect(() => {
+        const months = Object.keys(monthlyIncomeSummary);
+        if(months.length>0){
+            setIncomeMonth(months[0]);
+            setActiveIncomeMonth(months[0]);
+        }
+        else{
+            setIncomeMonth('');
+            setActiveIncomeMonth('');
+        }
+        console.log("Updated monthlyIncomeSummary:", monthlyIncomeSummary);
+    }, [monthlyIncomeSummary]);
+
+    useEffect(() => {
+        const months = Object.keys(monthlyExpenseSummary);
+        if(months.length>0){
+            setExpenseMonth(months[0]);
+            setActiveExpenseMonth(months[0]);
+        }
+        else{
+            setExpenseMonth('');
+            setActiveExpenseMonth('');
+        }
+        console.log("Updated monthlyExpenseSummary:", monthlyExpenseSummary);
+    }, [monthlyExpenseSummary]);
+    function handleMonthClick(month,type){
+        if(type=='income'){
+            setIncomeMonth(month);
+            setActiveIncomeMonth(month)
+            console.log("income month set as: ",month)
+        }
+        else{
+            setExpenseMonth(month);
+            setActiveExpenseMonth(month);
+            console.log("expense month set as: ",month)
+        }
+    }
   return (
     <>
     
@@ -55,32 +100,82 @@ function Analysis() {
         <div className='graph-content'> 
             {error ? ("Could Not Display the data :("):(
                 <>
-                    {/* <div className='income-category-wise'> */}
+                {
+                    Object.keys(incomeSummary).length &&
+                    <>
                         <p>income-category-wise</p>
                         <div className='pie-chart'>
                             <PieChart data={incomeSummary} label={"Total Income"}/>
                         </div>
-                    {/* </div> */}
-                    {/* <div className="expense-category-wise"> */}
+                    </>
+                }
+                {
+                    Object.keys(expenseSummary).length &&
+                    <>
                         <p>expense-category-wise</p>
                         <div className='pie-chart'>
                             <PieChart data={expenseSummary} label={"Total Expense"}/>
                         </div>
-                    {/* </div> */}
+                    </>
+                }
+                <div className="button-panel">
+                    {Object.keys(monthlyIncomeSummary).map((month,index)=>(
+                        <button key={index} 
+                        className={month === activeIncomeMonth ? 'active' : ''}
+                        onClick={()=> handleMonthClick(month,"income")
+                        }>
+                            {month}
+                        </button>
+                    ))}
+                </div>
+                <div className="doughnut-chart">
+                {
+                    incomeMonth && monthlyIncomeSummary[incomeMonth] &&
+                    <>
+                    <p>Income Distribution For {incomeMonth}</p>
+                    <DoughnutChart data={monthlyIncomeSummary[incomeMonth]}
+                    label={`Income Distribution for ${incomeMonth}`}/>
+                    </>
+                }
+                </div>
+                <div className="button-panel">
+                    {Object.keys(monthlyExpenseSummary).map((month,index)=>(
+                        <button key={index} 
+                        className={month === activeExpenseMonth ? 'active' : ''}
+                        onClick={()=> handleMonthClick(month,"expense")
+                        }>
+                            {month}
+                        </button>
+                    ))}
+                </div>
+                <div className="doughnut-chart">
+                {
+                    expenseMonth && monthlyExpenseSummary[expenseMonth] &&
+                    <>
+                    <p>Expense Distribution For {expenseMonth}</p>
+                    <DoughnutChart data={monthlyExpenseSummary[expenseMonth]}
+                    label={`Income Distribution for ${expenseMonth}`}/>
+                    </>
+                }
+                </div>
                 </>
             )}
         </div>
     </AnalysisStyled>
-    <GraphStyled>
-        <div className="income-distribution">
-            <p>Income-distribution</p>
-            <BarChart data={incomeDistribution} label={"Income"}/>
-        </div>
-        <div className="expense-distribution">
-            <p>Expense-distribution</p>
-            <BarChart data={expenseDistribution} label={"Expense"}/>
-        </div>
-    </GraphStyled>
+    {
+        error ? ("") :
+        <GraphStyled>
+            <div className="income-distribution">
+                <p>Income-distribution</p>
+                <BarChart data={incomeDistribution} label={"Income"}/>
+            </div>
+            <div className="expense-distribution">
+                <p>Expense-distribution</p>
+                <BarChart data={expenseDistribution} label={"Expense"}/>
+            </div>
+        </GraphStyled>
+    }
+    
     </>
   )
 }
@@ -97,6 +192,57 @@ const AnalysisStyled = styled.h1`
     }
     .pie-chart{
         width:50%;
+        margin-bottom: 2rem;
+    }
+    .button-panel {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        margin-bottom: 2rem;
+
+        button {
+            background: transparent;
+            // border: 1px solid transparent;
+            border: none;
+            color: var(--primary-color);
+            font-size: 1rem;
+            cursor: pointer;
+            margin: 0.5rem;
+            padding: 0.5rem 1rem;
+            position: relative;
+            overflow: hidden;
+            transition: color 0.3s;
+
+            &:hover {
+                font-weight: bold;
+                color: var(--primary-color); 
+                border-color: var(--primary-color);
+            }
+
+            &:after {
+                content: '';
+                position: absolute;
+                left: 0;
+                bottom: 0;
+                width: 100%;
+                height: 2px;
+                background: var(--primary-color);
+                transform: translateX(-100%);
+                transition: transform 0.6s;
+            }
+
+            &:hover:after {
+                transform: translateX(0);
+            }
+            &.active {
+                font-weight: bold;
+                border-bottom: 2px solid var(--primary-color);
+            }
+        }
+    }
+    .doughnut-chart{
+        width: 50%;
+        margin-bottom: 4rem;
     }
     
 `;
